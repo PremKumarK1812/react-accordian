af75930452efc13ce48acca9954b836dd0c407f2
PremKumarK1812 <121390534+PremKumarK1812@users.noreply.github.com>
2023-10-11 12:26:39 +0530

    minor changes

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -111,8 +111,8 @@ const SignUp = () => {
         const user = userCredential.user;
         setEmail('');
         setPassword('');
-        alert('Logged in Successfully!!!');
         navigate('/home');
+        alert('Signed up Successfully!!!');
         console.log(user);
       })
       .catch((err) => {
